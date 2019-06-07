c42b0b17eaa3f4c28bc072f0eebc374f953bd7bc
zeroAska <trotwoodray@gmail.com>
2019-06-07 16:27:57 -0400

    Add the bash file to start semantic mapping image

---
+++ semantic_mapping/run_cuda_docker.bash
@@ -0,0 +1,17 @@
+xhost +local:
+docker run -it --net=host --runtime=nvidia \
+  --user=$(id -u) \
+  -e DISPLAY=$DISPLAY \
+  -e QT_GRAPHICSSYSTEM=native \
+  -e CONTAINER_NAME=cuda \
+  -e USER=$USER \
+  --workdir=/home/$USER \
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  -v "/etc/group:/etc/group:ro" \
+  -v "/etc/passwd:/etc/passwd:ro" \
+  -v "/etc/shadow:/etc/shadow:ro" \
+  -v "/etc/sudoers.d:/etc/sudoers.d:ro" \
+  -v "/home/$USER/:/home/$USER/" \
+  --device=/dev/dri:/dev/dri \
+  --name=cuda-9.0 \
+  umrobotics/semantic_mapping:latest
