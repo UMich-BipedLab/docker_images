1ff2eecc7fcc4020984e2226e53523aa4b808ffe
zeroAska <trotwoodray@gmail.com>
2020-01-19 16:33:01 -0500

    add docker file for cvo

---
+++ cvo_gpu/run_cuda_docker.bash
@@ -0,0 +1,19 @@
+container_name=$1
+
+xhost +local:
+docker run -it --net=host  \
+  --user=$(id -u) \
+  -e DISPLAY=$DISPLAY \
+  -e QT_GRAPHICSSYSTEM=native \
+  -e NVIDIA_DRIVER_CAPABILITIES=all \
+  -e XAUTHORITY \
+  -e USER=$USER \
+  --workdir=/home/$USER/ \
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  -v "/etc/passwd:/etc/passwd:rw" \
+  -e "TERM=xterm-256color" \
+  -v "/home/$USER/code/docker_home:/home/$USER/" \
+  -v "/run/media/$USER/Samsung_T5/:/home/$USER/code/docker_home/media/Samsung_T5" \
+  --device=/dev/dri:/dev/dri \
+  --name=${container_name} \
+  umrobotics/cuda_arch:latest
