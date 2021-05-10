314a2795abf8262626f9d38c686860d4037e026d
zeroAska <trotwoodray@gmail.com>
2021-05-10 17:45:32 -0400

    update docker file

--- cvo_gpu/run_cuda_docker.bash
+++ cvo_gpu/run_cuda_docker.bash
@@ -2,24 +2,23 @@ container_name=$1
 
 xhost +local:
 docker run -it --net=host --gpus all  \               # this enable nvidia driver
-  --user=$(id -u) \
-  -e DISPLAY=$DISPLAY \
+  -e DISPLAY=${DISPLAY} \
   -e QT_GRAPHICSSYSTEM=native \
   -e NVIDIA_DRIVER_CAPABILITIES=all \
   -e XAUTHORITY \
-  -e USER=$USER \                                     # use the same username as you
+  -e USER=${USER} \                                     # use the same username as you
   --workdir=/home/$USER/ \
   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \           
   -v "/etc/passwd:/etc/passwd:rw" \        
   -e "TERM=xterm-256color" \
-<<<<<<< HEAD
+  --device /dev/nvidia0 --device /dev/nvidia-uvm --device /dev/nvidia-modeset --device /dev/nvidiactl --device /dev/nvidia-uvm-tools \
   -v "/home/$USER/code/docker_home:/home/$USER/" \
   -v "/run/media/$USER/Samsung_T5/:/home/$USER/media/Samsung_T5" \
   -v "/run/media/$USER/Samsung_T5_2/:/home/$USER/media/Samsung_T5_2" --name=${container_name}  umrobotics/cvo:latest
-=======
-  -v "/home/$USER/code/docker_home:/home/$USER/" \    # map the "/home/$USER/code/docker_home" outside docker as the docker container's home dir
-  -v "/run/media/$USER/Samsung_T5/:/home/$USER/media/Samsung_T5" \   # map the external SSD/HDD into docker container
-  --device=/dev/dri:/dev/dri \
-  --name=${container_name} \
-  umrobotics/cuda_arch:latest
->>>>>>> origin/master
+  #--user=$(id -u) \
+#  -v "/home/$USER/code/docker_home:/home/$USER/" \    # map the "/home/$USER/code/docker_home" outside docker as the docker container's home dir
+#  -v "/run/media/$USER/Samsung_T5/:/home/$USER/media/Samsung_T5" \   # map the external SSD/HDD into docker container
+#  --device=/dev/dri:/dev/dri \
+#  --name=${container_name} \
+#  umrobotics/cuda_arch:latest
+#>>>>>>> origin/master
